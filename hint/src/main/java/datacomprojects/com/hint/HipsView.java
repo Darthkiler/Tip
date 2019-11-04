73fab1ea53599f377c7198fb1ca5ec389a1e73d0
Adrian <vlad.adrian@mail.ru>
2019-11-04 17:43:40 +0200

    Закончил свою работу. Принимайте!!!

--- hint/src/main/java/datacomprojects/com/hint/HipsView.java
+++ hint/src/main/java/datacomprojects/com/hint/HipsView.java
@@ -1,6 +1,6 @@
 package datacomprojects.com.hint;
 
-
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Canvas;
@@ -10,16 +10,14 @@ import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
 import androidx.constraintlayout.widget.ConstraintLayout;
-
+import datacomprojects.com.roundbackground.RoundBackgroundLayout;
 import datacomprojects.com.tip.R;
 
 public class HipsView extends ConstraintLayout {
 
     private static final int CORNER_RADIUS = dpToPx(4);
-    private static final int CURSOR_MARGIN = dpToPx(22);
+    private static final int CURSOR_MARGIN = dpToPx(12);
     private static final int MIN_CURSOR_MARGIN_WITH_RADIUS = dpToPx(8);
     private static final int MARGIN_BETWEEN_CURSOR_AND_VIEW = dpToPx(5);
     private static final int CURSOR_HEIGHT = dpToPx(15);
@@ -34,27 +32,38 @@ public class HipsView extends ConstraintLayout {
     private Paint paint;
     private Path path;
     private boolean pointerPositionTop;
-    private View view;
+    private View firstView;
+    private RoundBackgroundLayout rbl;
 
 
     public HipsView(Context context) {
         super(context);
-        init(context,null);
+        init(context);
     }
 
     public HipsView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context, attrs);
+        init(context);
     }
 
     public HipsView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context, attrs);
+        init(context);
     }
 
 
-    private void init (Context context, AttributeSet attrs) {
+    @SuppressLint("ClickableViewAccessibility")
+    private void init (Context context) {
         View.inflate(context, R.layout.hips_view, this);
+
+
+        rbl = findViewById(R.id.roundBackgroundLayout);
+
+        setOnTouchListener((v, event) -> {
+            hide();
+            return false;
+        });
+
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setColor(Color.GREEN);
         path = new Path();
@@ -64,25 +73,6 @@ public class HipsView extends ConstraintLayout {
 
         // Тут надо брать значения из XML и ставить эти по дефолту.
         // Надо делать еще и проверку, чтобы cornerRadius + cursorMargin >= MIN_CURSOR_MARGIN_WITH_RADIUS
-
-
-
-        /*if(attrs != null) {
-            TypedArray typedArray = context.obtainStyledAttributes(attrs,R.styleable.HipsView);
-
-            TextView textView = findViewById(R.id.text);
-            RoundBackgroundLayout roundBackgroundLayout = findViewById(R.id.background);
-
-            textView.setText(typedArray.getString(R.styleable.HipsView_text));
-            textView.setTextColor(typedArray.getColor(R.styleable.HipsView_textColor,Color.WHITE));
-            //Typeface typeface = ResourcesCompat.getFont(context,typedArray.getResourceId(R.styleable.HipsView_font_Family,-1));
-            //textView.setTypeface(typeface);
-
-            roundBackgroundLayout.setBackground(typedArray.getColor(R.styleable.HipsView_backgroundColor, Color.BLACK));
-
-            typedArray.recycle();
-        }*/
-
         cornerRadius = CORNER_RADIUS;
         cursorMargin = CURSOR_MARGIN;
         marginBetweenCursorAndView = MARGIN_BETWEEN_CURSOR_AND_VIEW;
@@ -98,17 +88,10 @@ public class HipsView extends ConstraintLayout {
 
     public void show() {
         setVisibility(VISIBLE);
-        requestFocus();
-
-        setOnFocusChangeListener((v, hasFocus) -> {
-            if(!hasFocus)
-                hide();
-        });
     }
 
     public void hide() {
         setVisibility(INVISIBLE);
-        setOnFocusChangeListener(null);
     }
 
 
@@ -116,30 +99,31 @@ public class HipsView extends ConstraintLayout {
         return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
     }
 
+
     @Override
     public void dispatchDraw(Canvas canvas) {
         super.dispatchDraw(canvas);
 
-        if(view == null)
+        if(firstView == null)
             return;
 
-        int height = getMeasuredHeight() + cursorHeight + marginBetweenCursorAndView - getPaddingTop() - getPaddingBottom();
+        int height = rbl.getMeasuredHeight() + cursorHeight + marginBetweenCursorAndView;
 
         int [] viewLocation = new int[2];
-        view.getLocationInWindow(viewLocation);
+        firstView.getLocationInWindow(viewLocation);
 
         int [] parentLocation = new int[2];
         ((ViewGroup)getParent()).getLocationInWindow(parentLocation);
 
         if(pointerPositionTop) {
-            if (viewLocation[1] - parentLocation[1] + view.getMeasuredHeight() + height > ((ViewGroup) getParent()).getMeasuredHeight())
+            if (viewLocation[1] - parentLocation[1] + firstView.getMeasuredHeight() + height > ((ViewGroup) getParent()).getMeasuredHeight())
                 pointerPositionTop = false;
         } else if (viewLocation[1] - parentLocation[1] < height)
             pointerPositionTop = true;
 
-        float viewCenter = viewLocation[0] - parentLocation[0] + view.getMeasuredWidth() / 2f;
+        float viewCenter = viewLocation[0] - parentLocation[0] + firstView.getMeasuredWidth() / 2f;
         float parentCenter = ((ViewGroup) getParent()).getMeasuredWidth() / 2f;
-        float tipsCenter = getMeasuredWidth() / 2f;
+        float tipsCenter = rbl.getMeasuredWidth() / 2f;
 
         int padding = cornerRadius + cursorMargin + cursorHalfWidth;
 
@@ -151,35 +135,35 @@ public class HipsView extends ConstraintLayout {
             if(translationX < 0)
                 translationX += Math.min(-translationX, Math.min(cursorMargin, cornerRadius + cursorMargin - MIN_CURSOR_MARGIN_WITH_RADIUS));
         } else {
-            translationX = viewCenter - getMeasuredWidth() + Math.max(padding, parentCenter - viewCenter + tipsCenter);
+            translationX = viewCenter - rbl.getMeasuredWidth() + Math.max(padding, parentCenter - viewCenter + tipsCenter);
             // проверка если выходит за правый край
-            float goesOverRightEdge = translationX + getMeasuredWidth() - parentCenter * 2;
+            float goesOverRightEdge = translationX + rbl.getMeasuredWidth() - parentCenter * 2;
 
             if (goesOverRightEdge > 0)
                 translationX -= Math.min(goesOverRightEdge, Math.min(cursorMargin, cornerRadius + cursorMargin - MIN_CURSOR_MARGIN_WITH_RADIUS));
         }
 
-        setTranslationX(translationX);
+        rbl.setTranslationX(translationX);
 
-
-        float cursorX = viewCenter - translationX;
 
         path.reset();
 
         if(pointerPositionTop) {
-            setPadding(0, cursorHeight + marginBetweenCursorAndView, 0, 0);
-            setTranslationY(viewLocation[1] - parentLocation[1] + view.getMeasuredHeight());
 
-            path.moveTo(cursorX, marginBetweenCursorAndView);
-            path.lineTo(cursorX - cursorHalfWidth, cursorHeight + marginBetweenCursorAndView);
-            path.lineTo(cursorX + cursorHalfWidth, cursorHeight + marginBetweenCursorAndView);
+            int rblPaddingTop = cursorHeight + marginBetweenCursorAndView;
+            int translationY = viewLocation[1] - parentLocation[1] + firstView.getMeasuredHeight();
+            rbl.setTranslationY(translationY + rblPaddingTop);
+
+            path.moveTo(viewCenter, translationY + marginBetweenCursorAndView);
+            path.lineTo(viewCenter - cursorHalfWidth, translationY + cursorHeight + marginBetweenCursorAndView);
+            path.lineTo(viewCenter + cursorHalfWidth, translationY + cursorHeight + marginBetweenCursorAndView);
         } else {
-            setPadding(0, 0, 0, cursorHeight + marginBetweenCursorAndView);
-            setTranslationY(viewLocation[1] - parentLocation[1] - height);
+            int translationY = viewLocation[1] - parentLocation[1] - height;
+            rbl.setTranslationY(translationY);
 
-            path.moveTo(cursorX - cursorHalfWidth, getMeasuredHeight() - marginBetweenCursorAndView - cursorHeight);
-            path.lineTo(cursorX + cursorHalfWidth, getMeasuredHeight() - marginBetweenCursorAndView - cursorHeight);
-            path.lineTo(cursorX, getMeasuredHeight() - marginBetweenCursorAndView);
+            path.moveTo(viewCenter - cursorHalfWidth, translationY + rbl.getMeasuredHeight());
+            path.lineTo(viewCenter + cursorHalfWidth, translationY + rbl.getMeasuredHeight());
+            path.lineTo(viewCenter, translationY + rbl.getMeasuredHeight() + cursorHeight);
         }
 
         path.close();
@@ -187,8 +171,8 @@ public class HipsView extends ConstraintLayout {
 
     }
 
-    public void attachToView(View view) {
-        this.view = view;
-    }
 
+    public void attachToView(View firstView) {
+        this.firstView = firstView;
+    }
 }
